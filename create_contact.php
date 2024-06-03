51f55384955735b0d04a539d1a08f08f809b0d17
Your Name <you@example.com>
2024-06-03 12:38:49 +0200

    acabando web

--- create_contact.php
+++ create_contact.php
@@ -1,7 +1,8 @@
 <?php
+    include'db.php';
 
 
-IF($_SERVER['REQUEST_METHOD']=="POST"){
+if($_SERVER['REQUEST_METHOD']=="POST"){
 echo "CREANDO CONTACTO NUEVO <BR>";
 $nombre=$_POST['nombre'];
 $telefono=$_POST['telefono'];
@@ -13,7 +14,21 @@ echo "nombre: ".$nombre."<br>";
 echo "telefono: ".$telefono."<br>";
 echo "correo: ".$correo."<br>";
 echo "mensaje: ".$mensaje."<br>";
+$sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssss", $nombre, $telefono, $correo, $mensaje);
+$result = $stmt->execute();
+
+if($result){
+    $stmt->close();
+    $conn->close();
+    echo "CONTACTO CREADO";
+    header("Location:contacto.php?status=success&msg=Mensaje guardado!");
+}else{
+    echo "ERROR AL CREAR CONTACTO";
+}
 
 }else{
     echo "NO SE HA ENVIADO EL FORMULARIO";
+    header("Location:contacto.php?status=error&msg=Error guardando mensaje!");
 }
--- read.php
+++ read.php
@@ -1,8 +1,29 @@
-<h1>Mensajes</h1>
 <?php
-        include 'header.php';
+include 'db.php';
+include 'header.php';
+$sql = 'SELECT id, nombre, telefono, correo, mensaje FROM contactos';
+$result = $conn->query($sql);
+
+
+?>
+
+<h1>leer mensajes</h1>
+
+<div class="contenedor listado">
+    <?php
+    while($row = $result->fetch_assoc()){
     ?>
-<h1>Leer mensajes</h1>
-<?php
-        include 'footer.php';
+    <div class="fila">
+        <div class="campo"><?php echo $row['id']; ?></div>
+        <div class="campo"><?php echo $row['nombre']; ?></div>
+        <div class="campo"><?php echo $row['telefono']; ?></div>
+        <div class="campo"><?php echo $row['correo']; ?></div>
+        <div class="campo"><?php echo $row['mensaje']; ?></div>
+    </div>
+
+        <?php
+        }
     ?>
+</div>
+
+
